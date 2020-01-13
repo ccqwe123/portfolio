dfb9ef1e91e57fd4d7d9b308098db5ba6c4c6baf
unknown <jrc.jay38@gmail.com>
2020-01-13 13:47:39 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+  $('.button-group #btn1').click(function() {
+    $('#home').removeClass('hidden');
+    $('#about').addClass('hidden');
+    $('#skills').addClass('hidden');
+    $('#contact').addClass('hidden');
+    $('#experience').addClass('hidden');
+    $('.sidebar').addClass('hidden');
+  });
+  $('.button-group #btn2').click(function() {
+    $('#about').removeClass('hidden');
+    $('#skills').addClass('hidden');
+    $('#contact').addClass('hidden');
+    $('#experience').addClass('hidden');
+    $('#home').addClass('hidden');
+    $('.sidebar').removeClass('hidden');
+  });
+  $('.button-group #btn3').click(function() {
+    $('#skills').removeClass('hidden');
+    $('#about').addClass('hidden');
+    $('#contact').addClass('hidden');
+    $('#experience').addClass('hidden');
+    $('#home').addClass('hidden');
+    $('.sidebar').removeClass('hidden');
+  });
+  $('.button-group #btn4').click(function() {
+    $('#experience').removeClass('hidden');
+    $('#about').addClass('hidden');
+    $('#contact').addClass('hidden');
+    $('#skills').addClass('hidden');
+    $('.sidebar').removeClass('hidden');
+  });
+  $('.button-group #btn5').click(function() {
+    $('#contact').removeClass('hidden');
+    $('#about').addClass('hidden');
+    $('#skills').addClass('hidden');
+    $('#experience').addClass('hidden');
+    $('#home').addClass('hidden');
+    $('.sidebar').removeClass('hidden');
+  });
+  $('#home-about').click(function() {
+    $('.project-area .button-group #btn2').trigger('click');
+  });
+});
+$(document).ready(function () {
+
+
+    let $btns = $('.project-area .button-group button');
+
+
+    $btns.click(function (e) {
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+        // e.target.dataset.addClass('hidden');
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+            filter: selector
+        });
+        // console.log(selector +" "+e.target.dataset.filter);
+        var getElementID = selector.replace(/[,.]/g , ''); 
+        if(selector == e.target.dataset.filter)
+        {
+          // console.log(getElementID);
+          // $('.about').addClass('hidden');
+          // $('.skills').addClass('hidden');
+          // $('.contact').addClass('hidden');
+          // $('.experience').addClass('hidden');
+          $('.'+ getElementID).removeClass('hidden');
+          // alert("yes");
+        }
+        return false;
+    })
+
+    $('.project-area .button-group #btn1').trigger('click');
+    // Owl-carousel
+
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+        }
+    })
+
+    // sticky navigation menu
+
+});
